6a43df1d4d1b5217915df8963579f39843b208a9
Josh B <jabrackett40@gmail.com>
2020-02-26 20:21:32 -0800

    adjusted welcome message

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 Feb 26, 2020
 
 ### Project Title
-Post your work on Github
+Post your work on Github Project
 
 ### Description
-Add python bikeshare data to github repository.
+Add python bikeshare data to github repository. This program uses bikeshare data from three different cities.
 
 ### Files used
 chicago.csv, new_york_city.csv, washington.csv, bikeshare_2.py
--- bikeshare_2.py
+++ bikeshare_2.py
@@ -15,7 +15,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello there! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
 
@@ -48,7 +48,7 @@ def load_data(city, month, day):
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
-    print('\nCalculating The Most Frequent Times of Travel...\n')
+    print('\nCalculating the most frequent Times of Travel...\n')
     start_time = time.time()
 
     # display the most common month
@@ -60,14 +60,14 @@ def time_stats(df):
     # display the most common start hour
 
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
+    print("\nThis process took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
 
 def station_stats(df):
     """Displays statistics on the most popular stations and trip."""
 
-    print('\nCalculating The Most Popular Stations and Trip...\n')
+    print('\nCalculating the most popular Stations and Trip...\n')
     start_time = time.time()
 
     # display most commonly used start station
@@ -79,14 +79,14 @@ def station_stats(df):
     # display most frequent combination of start station and end station trip
 
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
+    print("\nThis process took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
 
 def trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
 
-    print('\nCalculating Trip Duration...\n')
+    print('\nCalculating the Trip Duration...\n')
     start_time = time.time()
 
     # display total travel time
@@ -95,14 +95,14 @@ def trip_duration_stats(df):
     # display mean travel time
 
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
+    print("\nThis process took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
 
 def user_stats(df):
     """Displays statistics on bikeshare users."""
 
-    print('\nCalculating User Stats...\n')
+    print('\nCalculating the User Stats...\n')
     start_time = time.time()
 
     # Display counts of user types
@@ -114,7 +114,7 @@ def user_stats(df):
     # Display earliest, most recent, and most common year of birth
 
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
+    print("\nThis process took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
 
